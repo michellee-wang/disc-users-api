b38029ec9d3a657006a06bad72abfc04d7d3e046
michellee-wang <michellewang857@gmail.com>
2025-12-19 19:23:29 -0500

    Add sample user data to schema.sql and change server port to 3001

--- schema.sql
+++ schema.sql
@@ -12,3 +12,9 @@ CREATE TABLE users (
     top_artists TEXT[],
     profile_picture TEXT
 );
+
+-- Sample data with random information
+INSERT INTO users (first_name, last_name, email, bio, major, graduation_year, top_artists, profile_picture) VALUES
+    ('Angelina', 'Xu', 'angelinaxu2029@u.northwestern.edu', 'Music lover and coffee addict ☕', 'Computer Science', 2025, ARRAY['Taylor Swift', 'Olivia Rodrigo', 'Sabrina Carpenter'], 'https://i.postimg.cc/vBp7jJC6/Image-from-Pinterest.jpg'),
+    ('Maggie', 'Yuan', 'maggieyuan2029@u.northwestern.edu', 'Always got my headphones on 🎧', 'Business Administration', 2024, ARRAY['Drake', 'Travis Scott', '21 Savage'], 'https://i.postimg.cc/vBp7jJC6/Image-from-Pinterest.jpg'),
+    ('Sophia', 'Wang', 'sophiawang8590@gmail.com', 'Indie vibes only ✨', 'Psychology', 2026, ARRAY['Arctic Monkeys', 'The Neighbourhood', 'Hozier'], 'https://i.postimg.cc/vBp7jJC6/Image-from-Pinterest.jpg');
